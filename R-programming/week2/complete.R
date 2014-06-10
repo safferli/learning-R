f937b366203439b18bccddf1812defd170971caf
Christoph Safferling <christoph.safferling@univie.ac.at>
2014-06-10 21:22:22 +0200

    working version of complete.R

--- R-programming/week2/complete.R
+++ R-programming/week2/complete.R
@@ -19,13 +19,29 @@
 workspace <- "/home/safferli/Documents/R-course/github/R-programming/week2/"
 setwd(workspace)
 
+## 'directory' is a character vector of length 1 indicating
+## the location of the CSV files
 
-complete <- function(directory, id = 1:332) {
-    ## 'directory' is a character vector of length 1 indicating
-    ## the location of the CSV files
-    
-    ## 'id' is an integer vector indicating the monitor ID numbers
-    ## to be used
+## 'id' is an integer vector indicating the monitor ID numbers
+## to be used
+complete <- function(directory, id = 1:332) {    
+    # get all filenames
+    files <- list.files(directory, pattern="csv")    
+    # initialise dataframe so that rbind() can append
+    total <- data.frame()
+    # loop to read only selected files, and directly parse the complete.cases()
+    for (i in id) {
+        # selected_data <- rbind(selected_data, read.csv(paste0(directory, "/", files[i])))
+        total <- rbind(total, 
+            # bind the two colums: id; nobs
+            cbind(
+                # name the id -- it's i!
+                as.integer(c(i)), 
+                # count the number of complete cases
+                sum(complete.cases(read.csv(paste0(directory, "/", files[i]))))
+                )#cbind
+            )#rbind
+    } # end for loop
     
     ## Return a data frame of the form:
     ## id nobs
@@ -34,26 +50,28 @@ complete <- function(directory, id = 1:332) {
     ## ...
     ## where 'id' is the monitor ID number and 'nobs' is the
     ## number of complete cases
-# there is a complete.cases() function
-
-}
+    
+    # correct output naming and returning
+    colnames(total) <- c("id","nobs")
+    total
+}# complete()
 
 
 
 
 ## testing with known output
-source("complete.R")
-complete("specdata", 1)
+#source("complete.R")
+#complete("specdata", 1)
 ##   id nobs
 ## 1  1  117
-complete("specdata", c(2, 4, 8, 10, 12))
+#complete("specdata", c(2, 4, 8, 10, 12))
 ##   id nobs
 ## 1  2 1041
 ## 2  4  474
 ## 3  8  192
 ## 4 10  148
 ## 5 12   96
-complete("specdata", 30:25)
+#complete("specdata", 30:25)
 ##   id nobs
 ## 1 30  932
 ## 2 29  711
@@ -61,6 +79,6 @@ complete("specdata", 30:25)
 ## 4 27  338
 ## 5 26  586
 ## 6 25  463
-complete("specdata", 3)
+#complete("specdata", 3)
 ##   id nobs
 ## 1  3  243
