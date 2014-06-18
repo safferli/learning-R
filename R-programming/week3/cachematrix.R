73946168ee8e7f0161ddcdafa0ef7e7c4b97dfa3
Christoph Safferling <christoph.safferling@bluebyte.de>
2014-06-18 19:01:05 +0200

    possible solution chachematrix.R

--- R-programming/week3/cachematrix.R
+++ R-programming/week3/cachematrix.R
@@ -33,3 +33,62 @@ cacheSolve <- function(x, ...) {
     x$setinverse(value)
     value
 }
+
+
+## solution could be: 
+# makeCacheMatrix: Creates a special "matrix" object that can cache its inverse
+# makeCacheMatrix() return a list of functions to:
+# 1. Set the value of the matrix
+# 2. Get the value of the matrix
+# 3. Set the value of the inverse
+# 4. Get the value of the inverse
+
+
+makeCacheMatrix <- function(x = matrix()) {
+    # inv will store the cached inverse matrix
+    inv <- NULL
+    # Setter for the matrix
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    # Getter for the matrix
+    get <- function() x
+    # Setter for the inverse
+    setinv <- function(inverse) inv <<- inverse
+    # Getter for the inverse
+    getinv <- function() inv
+    # Return the matrix with our newly defined functions
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
+
+
+# cacheSolve: Computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+# If the inverse has already been calculated (and the matrix has not changed),
+# then the cachesolve should retrieve the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+    inv <- x$getinv()
+    # If the inverse is already calculated, return it
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    # The inverse is not yet calculated, so we calculate it
+    data <- x$get()
+    inv <- solve(data, ...)
+    # Cache the inverse
+    x$setinv(inv)
+    # Return a matrix that is the inverse of 'x'
+    inv
+}
+
+
+
+# NOT RUN Example:
+# n <- 4
+# x <- matrix(rnorm(n^2), nrow = n) // Create a square matrix x
+# cx <- makeCacheMatrix(x) // Create our special matrix
+# cx$get() // Return the matrix
+# cacheSolve(cx) // Return the inverse
+# cacheSolve(cx) == solve(x)
