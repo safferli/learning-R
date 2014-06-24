03829da4a62d4ae4df2d747c0873b0e29e62e7cf
Christoph Safferling <christoph.safferling@bluebyte.de>
2014-06-24 16:02:11 +0200

    skeleton function

--- R-programming/week4/best.R
+++ R-programming/week4/best.R
@@ -28,9 +28,19 @@ names(outcome)
 outcome[, 11] <- as.numeric(outcome[, 11])
 
 hist(outcome[, 11])
-                
+             
+## assignment 4, first part starts here
 
-
+best <- function(state, outcome) {
+    ## Read outcome data
+    ## Check that state and outcome are valid
+    ## Return hospital name in that state with lowest 30-day death
+    ## rate
+}
+The function should check the validity of its arguments. If an invalid state value is passed to best, the
+function should throw an error via the stop function with the exact message "invalid state". If an invalid
+outcome value is passed to best, the function should throw an error via the stop function with the exact
+message "invalid outcome".
 
 
 
