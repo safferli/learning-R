001a2a10f5afee086d9825973e001ec066ff6a0f
Christoph Safferling <christoph.safferling@bluebyte.de>
2014-06-26 14:08:48 +0200

    added skeleton files

---
+++ R-programming/week4/rankall.R
@@ -0,0 +1,180 @@
+rm(list = ls()); gc(); gc()
+options(bitmapType='cairo')
+options(scipen = 999)# full numeric output
+#require(RODBC)
+# require(XML)
+require(data.table)
+# require(plyr)
+# require(dplyr)
+# require(reshape2)
+#require(zoo)
+#require(compiler)
+# require(xlsx)
+# require(ggplot2)
+# require(grid)
+# require(scales)
+# require(RColorBrewer)
+
+# Define your workspace: "X:/xxx/"
+workspace <- "D:/github/learning-R/R-programming/week4/"
+#workspace <- "/home/safferli/Documents/R-course/github/R-programming/week4/"
+setwd(workspace)
+
+## assignment 4, first part starts here
+
+best <- function(state, outcome) {
+    ## Read outcome data
+    keep_columns <- rep("NULL", times=46)
+    keep_columns[2] <- "character" # Hospital.Name
+    keep_columns[7] <- "factor" # State
+    keep_columns[c(11, 17, 23)] <- "character" # outcomes, numeric returns error
+    # data.table throws errors upon changing to as.numeric later
+    # we'll use data.frame for now
+    data <- read.csv("outcome-of-care-measures.csv", 
+                                colClasses = keep_columns,
+                                na.strings = "Not Available",
+                                stringsAsFactors = FALSE)
+    # http://stackoverflow.com/questions/3796266/change-the-class-of-many-columns-in-a-data-frame
+    # no idea why I need to do as.numeric(as.character()), as they *should* be stored as character in the first place!
+    # change columns 3,4,5 to numeric
+    cols = c(3,4,5)
+    data[, cols] = apply(data[, cols], 2, function(x) as.numeric(as.character(x)))
+    # data.table for sorting/subsetting magic!
+    data <- data.table(data, key = "State")
+    
+    ## Check that state and outcome are valid
+    # is state valid? 
+    if(!state %in% data$State) stop("invalid state")
+    # is outcome valid?
+    possible_outcomes <- c("heart attack", "heart failure", "pneumonia")
+    if(!outcome %in% possible_outcomes) stop("invalid outcome")
+    
+    ## Return hospital name in that state with lowest 30-day death rate
+    # get column number for outcome variable (+2, since outcomes start at col 3)
+    i <- match(outcome, possible_outcomes) + 2
+    # select only colums 1,2, and the outcome colum i; only keep correct state
+    selected_data <- subset(data, State == state, select = colnames(data)[c(1,2,i)])
+    setnames(selected_data, 3, "V3")
+    # return hospital name ([[1]]) from the ordered list of one: lowest death rate
+    selected_data[head(order(V3,Hospital.Name), n=1L)][[1]]
+}
+
+# remove start text from variable names: 
+# setnames(data, names(data), gsub('Hospital.30.Day.Death..Mortality..Rates.from.', '', names(data)))
+
+## with=F in data.table so that 2 references the column
+## data[, 2, with = TRUE]
+## data[, 2, with = FALSE]
+
+# mean(subset(data, Ozone>31 & Temp>90)$Solar.R)
+# As with [.data.frame, compound queries can be concatenated on one line; e.g.,
+#          
+#          DT[,sum(v),by=colA][V1<300][tail(order(V1))]
+#          # sum(v) by colA then return the 6 largest which are under 300
+
+
+
+
+Write a function called rankall that takes two arguments: an outcome name (outcome) and a hospital rank-
+    ing (num). The function reads the outcome-of-care-measures.csv file and returns a 2-column data frame
+containing the hospital in each state that has the ranking specified in num. For example the function call
+rankall("heart attack", "best") would return a data frame containing the names of the hospitals that
+are the best in their respective states for 30-day heart attack death rates. The function should return a value
+for every state (some may be NA). The first column in the data frame is named hospital, which contains
+the hospital name, and the second column is named state, which contains the 2-character abbreviation for
+the state name. Hospitals that do not have data on a particular outcome should be excluded from the set of
+hospitals when deciding the rankings.
+Handling ties. The rankall function should handle ties in the 30-day mortality rates in the same way
+that the rankhospital function handles ties.
+The function should use the following template.
+rankall <- function(outcome, num = "best") {
+    ## Read outcome data
+    ## Check that state and outcome are valid
+    ## For each state, find the hospital of the given rank
+    ## Return a data frame with the hospital names and the
+    ## (abbreviated) state name
+}
+NOTE: For the purpose of this part of the assignment (and for efficiency), your function should NOT call
+the rankhospital function from the previous section.
+The function should check the validity of its arguments. If an invalid outcome value is passed to rankall,
+the function should throw an error via the stop function with the exact message "invalid outcome". The num
+variable can take values "best", "worst", or an integer indicating the ranking (smaller numbers are better).
+If the number given by num is larger than the number of hospitals in that state, then the function should
+return NA.
+Here is some sample output from the function.
+4
+> source("rankall.R")
+> head(rankall("heart attack", 20), 10)
+hospital state
+AK
+<NA>
+    AK
+AL
+D W MCMILLAN MEMORIAL HOSPITAL
+AL
+AR
+ARKANSAS METHODIST MEDICAL CENTER
+AR
+AZ JOHN C LINCOLN DEER VALLEY HOSPITAL
+AZ
+CA
+SHERMAN OAKS HOSPITAL
+CA
+CO
+SKY RIDGE MEDICAL CENTER
+CO
+CT
+MIDSTATE MEDICAL CENTER
+CT
+DC
+<NA>
+    DC
+DE
+<NA>
+    DE
+FL
+SOUTH FLORIDA BAPTIST HOSPITAL
+FL
+> tail(rankall("pneumonia", "worst"), 3)
+hospital state
+WI MAYO CLINIC HEALTH SYSTEM - NORTHLAND, INC
+WI
+WV
+PLATEAU MEDICAL CENTER
+WV
+WY
+NORTH BIG HORN HOSPITAL DISTRICT
+WY
+> tail(rankall("heart failure"), 10)
+hospital state
+TN
+WELLMONT HAWKINS COUNTY MEMORIAL HOSPITAL
+TN
+TX
+FORT DUNCAN MEDICAL CENTER
+TX
+UT VA SALT LAKE CITY HEALTHCARE - GEORGE E. WAHLEN VA MEDICAL CENTER
+UT
+VA
+SENTARA POTOMAC HOSPITAL
+VA
+VI
+GOV JUAN F LUIS HOSPITAL & MEDICAL CTR
+VI
+VT
+SPRINGFIELD HOSPITAL
+VT
+WA
+HARBORVIEW MEDICAL CENTER
+WA
+WI
+AURORA ST LUKES MEDICAL CENTER
+WI
+WV
+FAIRMONT GENERAL HOSPITAL
+WV
+WY
+CHEYENNE VA MEDICAL CENTER
+WY
+Save your code for this function to a file named rankall.R.
+
